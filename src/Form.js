ff48665d2dc1ef4720f4d6c74f6465f47cd271c9
ushamacdonald <usha.macdonald@gmail.com>
2018-09-21 11:06:03 +1200

    Able to log location coordinates

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        var location = {
+          longitude: position.coords.longitude,
+          latitude: position.coords.latitude
+        };
+        console.log(location);
+      });
+    } else {
+      console.log('Geo Location not supported by browser');
+    }
+
+    // console.log("click");
+    // navigator.geolocation.getCurrentPosition(() => {console.log("success")}, () => {console.log("fail")})
+  }
 
   render() {
     return (
       <div>
         <p>this is a form</p>
+        <button onClick={this.getLocation}>Get location</button>
       </div>
     );
   }
